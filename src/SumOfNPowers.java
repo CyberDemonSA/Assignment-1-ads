fde07726af4a49d0fe7ee39bdf85e05a9a5eb85d
User <230519@astanait.edu.kz>
2024-04-05 09:41:09 +0500

    1

--- src/SumOfNPowers.java
+++ src/SumOfNPowers.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
 public class SumOfNPowers {
-    public static int sumOfNPowers(int b, int n, int s ) {
+    public static int powersOfN(int b, int n) {
         if (n == 0) return 1;
         else{
-            System.out.println(b);
-            s = s + b;
-            b = b * sumOfNPowers(b, n - 1, s);
-            return b;
+            return b * powersOfN(b, n - 1);
+        }
+    }
+    public static int sumOfNPowers(int b, int n) {
+        if (n == 0) return powersOfN(b, n);
+        else{
+            return powersOfN(b, n) + sumOfNPowers(b, n - 1);
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(sumOfNPowers(a, b, 1));
+        System.out.println(sumOfNPowers(a, b));
     }
 }
